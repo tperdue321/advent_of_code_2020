990bf5990dc451d0d010356c53d849bfbee67416
Travis Perdue <travis.perdue@drip.com>
2020-12-01 22:00:16 -0600

    solve puzzle 2

--- day_1_puzzle_2_solution.rb
+++ day_1_puzzle_2_solution.rb
@@ -3,7 +3,10 @@ data = file.readlines.map(&:chomp).map(&:to_i)
 
 data.each do |x|
   data.each do |y|
-    @x = x * y if x + y == 2020
+    data.each do |z|
+      @x = x * y *z if x + y + z == 2020
+      break if !@x.nil?
+    end
     break if !@x.nil?
   end
   break if !@x.nil?
