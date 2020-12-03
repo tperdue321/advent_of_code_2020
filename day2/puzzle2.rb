d665a8091d1566c5e559dabd7cc57ccf8b133efd
Travis Perdue <travis.perdue@drip.com>
2020-12-02 22:27:45 -0600

    different file reading style

--- day2/puzzle2.rb
+++ day2/puzzle2.rb
@@ -1,11 +1,7 @@
-file = File.open("input.txt")
-data = file.readlines(&:chomp).map { |line| line.scan(/(\d+).(\d+)\s([a-zA-Z]+):\s([a-zA-Z]+)/).flatten }
-
 valid_count = 0
-
-data.each do |array|
+IO.foreach("input.txt") do |line|
+  array = line.scan(/(\d+).(\d+)\s([a-zA-Z]+):\s([a-zA-Z]+)/).flatten
   first, second, letter, password = array
   valid_count += 1 if (password[first.to_i-1] == letter) ^ (password[second.to_i-1] == letter)
 end
-
 puts valid_count
