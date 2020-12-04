4cce8edc080275203092a5e2387febb0e03faba1
Travis Perdue <travis.perdue@drip.com>
2020-12-03 21:44:59 -0600

    solve puzzle2 quick hack

--- day3/puzzle2.rb
+++ day3/puzzle2.rb
@@ -1,20 +1,31 @@
-trees_hit = 0
-position = 0
+# [right, down]
+angles = [[1,1],[3,1],[5,1],[7,1],[1,2]]
+
 
 def tree?(char)
   char == "#"
 end
 
-def calc_position(position, line_length)
-  position += 3
+def calc_position(position, line_length, rightshift)
+  position += rightshift
   position -= line_length if position >= line_length
   position
 end
 
-IO.foreach("input.txt") do |line|
-  line = line.chomp
-  trees_hit += 1 if tree?(line[position])
-  position = calc_position(position, line.length)
+all_trees_hit = []
+angles.each do |angle|
+  rightshift, downshift = angle
+  trees_hit = 0
+  position = 0
+  line_num = 1
+  IO.foreach("input.txt") do |line|
+    line_num += 1
+    next if (line_num % downshift) != 0
+    line = line.chomp
+    trees_hit += 1 if tree?(line[position])
+    position = calc_position(position, line.length, rightshift)
+  end
+  all_trees_hit << trees_hit
 end
 
-puts trees_hit
+puts all_trees_hit.reduce(:*)
