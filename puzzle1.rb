eeede0de9456e5d46f8704cd9a53f82e75ed36b0
Travis Perdue <travis.perdue@drip.com>
2020-12-05 02:35:14 -0600

    this took an embarrassingly long amount of time to solve

--- puzzle1.rb
+++ puzzle1.rb
@@ -2,7 +2,6 @@ require 'pry'
 pp(begin
   id = 0
   IO.foreach("input.txt") do |line|
-  # line = "FBFBBFFRLR"
     final_row = final_column = fr = l = 0; br = 127; r = 7
     line.chomp.each_char do |chr|
       if chr == "F"
@@ -12,9 +11,9 @@ pp(begin
         final_row = fr + 1
       elsif chr == "L"
         r = (l + r) / 2
-        final_column = r + 1
       else
         final_column = l = (l + r) / 2
+        final_column = l + 1
       end
     end
 
